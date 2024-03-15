2881662a61434db6e19b95934120ab1e214b8d1a
kalyanpothabathula <103244648+kalyanpothabathula@users.noreply.github.com>
2024-03-15 20:31:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'E-world & Elastic Beanstalk sample deploy ment'
+    title: 'E-world & Elastic Beanstalk sample code deloyment K@LY@N'
     })
     res.status(200).end();
 })
